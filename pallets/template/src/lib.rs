1322e34f16dffe5b1ff2d80946fc3bdc179f6731
ndkazu <ndongmefane@gmail.com>
2022-03-01 04:31:16 -0800

    test

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -10,14 +10,20 @@ mod mock;
 
 #[cfg(test)]
 mod tests;
+mod roles;
+pub use crate::roles::*;
 
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
+	use super::*;
+	use frame_support::{
+		dispatch::DispatchResult,
+		pallet_prelude::*,		
+	};
+	use frame_system::{ensure_signed, pallet_prelude::*};
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -57,6 +63,8 @@ pub mod pallet {
 		StorageOverflow,
 	}
 
+	type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
 	// These functions materialize as "extrinsics", which are often compared to transactions.
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
@@ -65,11 +73,12 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		pub fn do_something(origin: OriginFor<T>, something: u32, acc:AccountIdOf<T>) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://docs.substrate.io/v3/runtime/origins
 			let who = ensure_signed(origin)?;
+			let _inv = Investor::new(acc,something);
 
 			// Update storage.
 			<Something<T>>::put(something);
---
+++ pallets/template/src/roles.rs
@@ -0,0 +1,23 @@
+
+pub struct Investor<T,U>{
+    account_id:T,
+    nft:U,
+}
+impl<T,U> Investor<T,U>{
+    pub fn new(acc: T,nft:U)-> Investor<T,U>{
+        Investor{
+            account_id: acc,
+            nft: nft,
+        }
+        
+    }
+}
+
+pub struct HouseOwner<T,U>{
+    account_id:T,
+    nft:U,
+}
+pub struct Tenant<T,U>{
+    account_id:T,
+    rent:U,
+}
