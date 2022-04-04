0c5ed91205059ecf916cbf8e2edaf231cc9666ab
Kazunobu Ndong <33208377+ndkazu@users.noreply.github.com>
2022-04-04 14:54:40 +0900

    For ContributionsLog storage future modifications
    Added a field age: block number, which represents the account creation date. this will allow us to account for seniority among Investors.
    Also added a ContributionIndex variable to prepare for the following:
    The storage ContributionsLog will be changed from {account_id: Balance} to {ContributionIndex: (Balance,BlockNumber, Investor)}.
    Each Contribution/transaction is stored with the contribution amount, time of contribution, and Investor information.
    After applying this Commit, the following will be done:
    - Modify ContributionsLog in lib.rs
    - Modify the 'contribute' method in mod.rs to account for the new storage structure.

--- pallets/template/src/roles/mod.rs
+++ pallets/template/src/roles/mod.rs
@@ -4,8 +4,10 @@ pub use super::*;
 pub use crate::roles::items::*;
 pub type BalanceOf<T> = <<T as pallet::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+pub type BlockNumberOf<T> = <T as frame_system::Config>::BlockNumber;
 pub type Contributors<T> = Vec<AccountIdOf<T>>;
 pub type HouseIndex = u32;
+pub type ContributionIndex = u32;
 pub type Bool = bool;
 
 
@@ -14,12 +16,15 @@ pub type Bool = bool;
 pub struct Investor<T:Config,U>{
     pub account_id:AccountIdOf<T>,
     pub nft:U,
+    pub age:BlockNumberOf<T>,
 }
 impl<T:Config,U> Investor<T,U>{
     pub fn new(acc:AccountIdOf<T>,nft:U)-> Self{
+	now = let now = <frame_system::Pallet<T>>::block_number();
         Investor{
             account_id: acc,
             nft: nft,
+	    age: now,		
         }        
     }
 
@@ -30,7 +35,7 @@ impl<T:Config,U> Investor<T,U>{
     pub fn contribute(origin:OriginFor<T>,acc:AccountIdOf<T> ,value:BalanceOf<T>) -> DispatchResult{
         let c1=Contribution::new(&acc,&value);
         let who = ensure_signed(origin)?;
-		let now = <frame_system::Pallet<T>>::block_number();
+	let now = <frame_system::Pallet<T>>::block_number();
         if ContributionsLog::<T>::contains_key(c1.account){
             ContributionsLog::<T>::mutate(c1.account, |val|{
                 *val += *c1.amount;
