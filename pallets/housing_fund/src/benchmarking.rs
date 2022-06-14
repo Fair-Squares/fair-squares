7097b8ba1754ee509f9ae0d6f327e71c1aca7038
Stephen Meyo <meyo79@hotmail.com>
2022-06-14 15:05:37 +0200

    removed benchmark for housing_fund pallet

--- pallets/housing_fund/src/benchmarking.rs
+++ pallets/housing_fund/src/benchmarking.rs
@@ -6,16 +6,3 @@ use super::*;
 use crate::Pallet as Template;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
 use frame_system::RawOrigin;
-
-benchmarks! {
-	withdraw_fund {
-		let caller: T::AccountId = whitelisted_caller();
-		let value: u32 = 1000;
-		let amount: BalanceOf<T> = value.into();
-	}: _(RawOrigin::Signed(caller), amount)
-	verify {
-		assert_eq!(true, true);
-	}
-
-	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
-}
