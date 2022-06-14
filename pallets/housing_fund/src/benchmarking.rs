8582d99e0f02a5a00ce25a7b3db2d7d34a153822
Meyo Stpehn <90961928+letodunc@users.noreply.github.com>
2022-06-14 13:20:30 +0200

    completed contribute and withdraw functions (#45)
    adding contribute and withdraw function to housing fund

--- pallets/housing_fund/src/benchmarking.rs
+++ pallets/housing_fund/src/benchmarking.rs
@@ -1,9 +1,9 @@
-//! Benchmarking setup for pallet-housing_fund
+//! Benchmarking setup for pallet-template
 
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as HousingFund;
+use crate::Pallet as Template;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
 use frame_system::RawOrigin;
 
@@ -16,5 +16,5 @@ benchmarks! {
 		assert_eq!(Something::<T>::get(), Some(s));
 	}
 
-	impl_benchmark_test_suite!(HousingFund, crate::mock::new_test_ext(), crate::mock::Test);
+	impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test);
 }
