caf210640f1fec2766cfbeb0103c0edaefb01d97
letodunc <meyo79@hotmail.com>
2022-07-19 11:14:02 +0200

    added a script to add a new pallet from a pallet template and configure the runtime Cargo.toml and lib.rs

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -49,6 +49,7 @@ pub use sp_runtime::{Perbill, Permill};
 //import fs-pallets
 pub use pallet_housing_fund;
 pub use pallet_roles;
+// flag add pallet use
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -334,6 +335,8 @@ impl pallet_housing_fund::Config for Runtime {
 	type MaxInvestorPerHouse = MaxInvestorPerHouse;
 }
 
+// flag add pallet config
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub enum Runtime where
@@ -353,6 +356,7 @@ construct_runtime!(
 		// Include the custom logic from the pallet-template in the runtime.
 		RoleModule: pallet_roles,
 		HousingFundModule: pallet_housing_fund,
+		// flag add pallet runtime
 	}
 );
 
@@ -400,6 +404,7 @@ mod benches {
 		[pallet_uniques, Uniques]
 		[pallet_roles, RoleModule]
 		[pallet_housing_fund, HousingFundModule]
+		// flag add pallet bench_macro
 	);
 }
 
@@ -582,6 +587,7 @@ impl_runtime_apis! {
 			add_benchmarks!(params, batches);
 			add_benchmark!(params, batches, pallet_roles, RoleModule);
 			add_benchmark!(params, batches, pallet_housing_fund, HousingFundModule);
+			// flag add pallet benchmark
 
 			Ok(batches)
 		}
