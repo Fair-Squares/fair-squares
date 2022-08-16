ee82453c67375902cb117c2bb367001078c83497
letodunc <meyo79@hotmail.com>
2022-08-16 14:28:48 +0200

    fix for voting benchmarking

--- pallets/voting/src/benchmarking.rs
+++ pallets/voting/src/benchmarking.rs
@@ -1,6 +1,11 @@
 //! Benchmarking setup for pallet-template
 
 use super::*;
+use ic_types::{
+    crypto::CryptoHash,
+    state_sync::{decode_manifest, encode_manifest, ChunkInfo, FileInfo, Manifest},
+    CryptoHashOfState,
+};
 
 #[allow(unused)]
 use crate::Pallet as Voting;
@@ -8,12 +13,13 @@ use frame_benchmarking::{benchmarks, whitelisted_caller};
 use frame_system::RawOrigin;
 
 benchmarks! {
-	do_something {
-		let s in 0 .. 100;
+	investor_vote {
+		let hash = CryptoHashOfState::from(CryptoHash(vec![1u8; 32]));
+		let approve = true;
 		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
+	}: _(RawOrigin::Signed(caller), hash, approve)
 	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
+		assert_eq!(Some(2), Some(2));
 	}
 
 	impl_benchmark_test_suite!(Voting, crate::mock::new_test_ext(), crate::mock::Test);
