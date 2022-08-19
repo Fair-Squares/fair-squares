ced1bf5336e3d0a4d85875a13eea803556d925e1
Kazunobu Ndong <33208377+ndkazu@users.noreply.github.com>
2022-08-19 10:39:05 +0900

    Update benchmarking.rs

--- pallets/voting/src/benchmarking.rs
+++ pallets/voting/src/benchmarking.rs
@@ -9,21 +9,5 @@ use crate::Pallet as Voting;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
 use frame_system::RawOrigin;
 use frame_system::{Call as SystemCall};
+
 use pallet_roles::Hash;
-
-
-benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
-	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
-	}
-
-	impl_benchmark_test_suite!(Voting, crate::mock::new_test_ext(), crate::mock::Test);
-}
-
-
-
-
