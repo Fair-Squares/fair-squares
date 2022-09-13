a4824d17b382000489408da48e4828ed5bc434d6
ndkazu <ndongmefane@gmail.com>
2022-09-13 06:58:20 -0700

    virtual account

--- pallets/share_distributor/src/tests.rs
+++ pallets/share_distributor/src/tests.rs
@@ -1,20 +1,82 @@
-use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok};
+pub use super::*;
+pub use crate::mock::*;
+pub use frame_support::{assert_noop, assert_ok};
 
-#[test]
-fn it_works_for_default_value() {
-	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
-	});
+pub fn prep_roles() {
+	RoleModule::set_role(Origin::signed(CHARLIE).clone(), CHARLIE, Acc::SERVICER).ok();
+	RoleModule::account_approval(Origin::signed(ALICE), CHARLIE).ok();
+	RoleModule::set_role(Origin::signed(EVE).clone(), EVE, Acc::SERVICER).ok();
+	RoleModule::account_approval(Origin::signed(ALICE), EVE).ok();
+	RoleModule::set_role(Origin::signed(BOB).clone(), BOB, Acc::SELLER).ok();
+	RoleModule::account_approval(Origin::signed(ALICE), BOB).ok();
+	RoleModule::set_role(Origin::signed(DAVE).clone(), DAVE, Acc::INVESTOR).ok();
+	RoleModule::set_role(
+		Origin::signed(ACCOUNT_WITH_NO_BALANCE0).clone(),
+		ACCOUNT_WITH_NO_BALANCE0,
+		Acc::SERVICER,
+	)
+	.ok();
+	RoleModule::account_approval(Origin::signed(ALICE), ACCOUNT_WITH_NO_BALANCE0).ok();
 }
 
 #[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
+fn virtual0(){
+	ExtBuilder::default().build().execute_with(|| {
+		let metadata0: BoundedVec<u8, <Test as pallet_uniques::Config>::StringLimit> =
+		b"metadata0".to_vec().try_into().unwrap();
+	let metadata1: BoundedVec<u8, <Test as pallet_uniques::Config>::StringLimit> =
+		b"metadata1".to_vec().try_into().unwrap();
+	let metadata2: BoundedVec<u8, <Test as pallet_uniques::Config>::StringLimit> =
+		b"metadata2".to_vec().try_into().unwrap();
+	prep_roles();
+	//Charlie creates a collection
+	assert_ok!(NftModule::create_collection(
+		Origin::signed(CHARLIE),
+		NftColl::OFFICESTEST,
+		metadata0.clone()
+	));
+	//Charlie creates a second collection
+	assert_ok!(NftModule::create_collection(
+		Origin::signed(CHARLIE),
+		NftColl::APPARTMENTSTEST,
+		metadata0
+	));
+	// Bob creates a proposal without submiting for review
+	let price = 100_000_000;
+	assert_ok!(OnboardingModule::create_and_submit_proposal(
+		Origin::signed(BOB),
+		NftColl::OFFICESTEST,
+		Some(price.clone()),
+		metadata1,
+		false
+	));
+
+	
+
+		let coll_id0 = NftColl::OFFICESTEST.value();
+		let item_id0 = pallet_nft::ItemsCount::<Test>::get()[coll_id0 as usize] - 1;
+
+		assert_ok!(ShareDistributor::virtual_account(coll_id0,item_id0));
+
+		// Bob creates a second proposal without submiting for review
+	let price = 100_000_000;
+	assert_ok!(OnboardingModule::create_and_submit_proposal(
+		Origin::signed(BOB),
+		NftColl::APPARTMENTSTEST,
+		Some(price.clone()),
+		metadata2,
+		false
+	));
+		let coll_id1 = NftColl::APPARTMENTSTEST.value();
+		let item_id1 = pallet_nft::ItemsCount::<Test>::get()[coll_id1 as usize] - 1;
+
+		assert_ok!(ShareDistributor::create_virtual(Origin::signed(EVE),coll_id1,item_id1));
+		let virtual0 = Virtual::<Test>::get(coll_id0,item_id0).unwrap();
+		
+		let virtual1 = Virtual::<Test>::get(coll_id1,item_id1).unwrap();
+		println!("Virtual account nbr1:{:?}\nVirtual account nbr2:{:?}",virtual0,virtual1);
+		assert_ne!(virtual0.virtual_account,virtual1.virtual_account);
+
+
 	});
 }
