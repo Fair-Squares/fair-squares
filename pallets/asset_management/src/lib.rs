304891660a92574a0dd5d2fc1baea441c4495258
Kazunobu Ndong <ndongmefane@gmail.com>
2022-11-15 12:38:47 +0900

    Remove unused imports

--- pallets/asset_management/src/lib.rs
+++ pallets/asset_management/src/lib.rs
@@ -31,8 +31,6 @@ mod tests;
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
-	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
 	use frame_system::WeightInfo;
 
 	#[pallet::pallet]
