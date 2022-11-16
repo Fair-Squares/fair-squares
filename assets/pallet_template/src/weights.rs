3f601faf244c426f6e59e0221c7d8d0ec2a96413
letodunc <meyo79@hotmail.com>
2022-11-16 11:46:22 +0100

    updated pallet template files

--- assets/pallet_template/src/weights.rs
+++ assets/pallet_template/src/weights.rs
@@ -61,8 +61,8 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 	// Storage: TemplateModule Something (r:0 w:1)
 	/// The range of component `s` is `[0, 100]`.
 	fn do_something(_s: u32, ) -> Weight {
-		(15_009_000 as Weight)
-			.saturating_add(T::DbWeight::get().writes(1 as Weight))
+		Weight::from_ref_time(15_009_000 as u64)
+			.saturating_add(T::DbWeight::get().writes(1 as u64))
 	}
 }
 
@@ -71,7 +71,7 @@ impl WeightInfo for () {
 	// Storage: TemplateModule Something (r:0 w:1)
 	/// The range of component `s` is `[0, 100]`.
 	fn do_something(_s: u32, ) -> Weight {
-		(15_009_000 as Weight)
-			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
+		Weight::from_ref_time(15_009_000 as u64)
+			.saturating_add(RocksDbWeight::get().writes(1 as u64))
 	}
 }
