1c259aa88bbffa809800f1e022fbea5ed5cf988a
Kazunobu Ndong <ndongmefane@gmail.com>
2023-01-22 23:47:17 +0900

    Additional test for payment pallet: payment_release

--- pallets/payment/src/tests.rs
+++ pallets/payment/src/tests.rs
@@ -144,3 +144,74 @@ fn test_cancel_works() {
 		assert_eq!(PaymentStore::<Test>::get(PAYMENT_CREATOR, PAYMENT_RECIPENT), None);
 	});
 }
+
+#[test]
+fn test_release_works() {
+	new_test_ext().execute_with(|| {
+		let creator_initial_balance = 100_000_000_000 as u64;
+		let payment_amount = 40 as u64;
+		let expected_incentive_amount = payment_amount / INCENTIVE_PERCENTAGE as u64;
+		let initial_recipient_balance = Balances::free_balance(&PAYMENT_RECIPENT);
+
+		// should be able to create a payment with available balance
+		assert_ok!(PaymentModule::pay(
+			Origin::signed(PAYMENT_CREATOR),
+			PAYMENT_RECIPENT,
+			payment_amount,
+			None
+		));
+		assert_eq!(
+			PaymentStore::<Test>::get(PAYMENT_CREATOR, PAYMENT_RECIPENT),
+			Some(PaymentDetail {
+				amount: payment_amount,
+				incentive_amount: expected_incentive_amount,
+				state: PaymentState::Created,
+				resolver_account: RESOLVER_ACCOUNT,
+				fee_detail: Some((FEE_RECIPIENT_ACCOUNT, 0)),
+			})
+		);
+		// the payment amount should be reserved
+		assert_eq!(
+			Balances::free_balance(&PAYMENT_CREATOR),
+			creator_initial_balance - payment_amount - expected_incentive_amount
+		);
+		assert_eq!(Balances::free_balance(&PAYMENT_RECIPENT), 1);
+
+		// should succeed for valid payment
+		assert_ok!(PaymentModule::release(
+			Origin::signed(PAYMENT_CREATOR),
+			PAYMENT_RECIPENT
+		));
+		assert_eq!(
+			last_event(),
+			crate::Event::<Test>::PaymentReleased {
+				from: PAYMENT_CREATOR,
+				to: PAYMENT_RECIPENT
+			}
+			.into()
+		);
+		// the payment amount should be transferred
+		assert_eq!(
+			Balances::free_balance(&PAYMENT_CREATOR),
+			creator_initial_balance - payment_amount
+		);
+		assert_eq!(Balances::free_balance(&PAYMENT_RECIPENT), payment_amount+initial_recipient_balance);
+
+		// should be deleted from storage
+		assert_eq!(PaymentStore::<Test>::get(PAYMENT_CREATOR, PAYMENT_RECIPENT), None);
+
+		// should be able to create another payment since previous is released
+		assert_ok!(PaymentModule::pay(
+			Origin::signed(PAYMENT_CREATOR),
+			PAYMENT_RECIPENT,
+			payment_amount,
+			None
+		));
+		// the payment amount should be reserved
+		assert_eq!(
+			Balances::free_balance(&PAYMENT_CREATOR),
+			creator_initial_balance - (payment_amount * 2) - expected_incentive_amount
+		);
+		assert_eq!(Balances::free_balance(&PAYMENT_RECIPENT), payment_amount+initial_recipient_balance);
+	});
+}
