cc77402ace612838babc413f0a0a6a7914a60fe9
Ilhan <29432367+ilhanu@users.noreply.github.com>
2023-04-06 16:03:05 +0200

    Testnet genesis changes (#266)
    * custom_specs.json
    
    * voting time, unlock systemCalls for council and democracy
    
    * Added square_one to chain_spec
    
    * Fixed typo
    
    * add telemetry
    
    * changes to chain-spec include square_one
    
    * Fixed compilation error
    
    * Roles and council settings
    
    * Faucet
    
    * reverted some changes in chainspec
    
    ---------
    
    Co-authored-by: Kazunobu Ndong <ndongmefane@gmail.com>

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -8,9 +8,13 @@ use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{sr25519, Pair, Public};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{IdentifyAccount, Verify};
+use sc_telemetry::TelemetryEndpoints;
+use hex_literal::hex;
+
 
 // The URL for the telemetry server.
-// const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
+const POLKADOT_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
+
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
@@ -74,6 +78,40 @@ pub fn get_endowed_accounts_with_balance() -> Vec<(AccountId, u128)> {
 		for tup2 in additional_accounts_with_balance.iter() {
 			if tup1.0 == tup2.0 {
 				return
+			}
+		}
+		accounts.push(tup1.to_owned());
+	});
+
+	accounts
+}
+
+fn testnet_endowed_accounts() -> Vec<(AccountId,u128)> {
+	let accounts: Vec<AccountId> = vec![
+		//5CcZ9b6Wezpos7BBfcN3o4Jam6NQ8tU4gb4Xa7roMCCyND5X
+		hex!["184a89cbb6aa857b41c98841be365ab3947ef1f729aa6fe0f6a1322f6391945b"].into(),
+
+		//5D1nJ8M862utBuDG7mwszhm1o7Tzn5FSd6BYEoX3uc5e3AuB
+		hex!["2a0170a78af6835dd46753c1857b31903aa125d9c203e05bc7a45b7c3bea702b"].into(),
+
+		//5FnsQ9tHAjqbb1YHU4hAr2hPgDS82XGvNPA35CpV7hX58sVv ==> Faucet
+		hex!["a4dd21a5be2a4d85b80a0091f77b3371fa3c4f3cf511bead59cf65583251ce16"].into(),
+
+		//5Ferj4SHg8mtUGxWHuyjhciTCmh7TAyhw8pSjFtxRAJDynpk 
+		hex!["9ec0e63219270075ffd546e4fa39b4027216a9de5ed16b38bc54d66fe09b8d47"].into(),
+	];
+	let accounts_with_balance: Vec<(AccountId, u128)> =
+		accounts.iter().cloned().map(|k| (k, 1 << 60)).collect();
+	let json_data = &include_bytes!("../../seed/balances.json")[..];
+	let additional_accounts_with_balance: Vec<(AccountId, u128)> =
+		serde_json::from_slice(json_data).unwrap();
+
+	let mut accounts = additional_accounts_with_balance.clone();
+
+	accounts_with_balance.iter().for_each(|tup1| {
+		for tup2 in additional_accounts_with_balance.iter() {
+			if tup1.0 == tup2.0 {
+				return;
 			}
 		}
 		accounts.push(tup1.to_owned());
@@ -160,6 +198,44 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	))
 }
 
+pub fn square_one_testnet() -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+
+	Ok(ChainSpec::from_genesis(
+		// Name
+		"Fair Squares testnet",
+		// ID
+		"square-one",
+		ChainType::Live,
+		move || {
+			square_one(
+				wasm_binary,
+				// Initial PoA authorities
+				vec![authority_keys_from_seed("Alice"), authority_keys_from_seed("Bob")],
+				// Sudo account
+				hex!["2a0170a78af6835dd46753c1857b31903aa125d9c203e05bc7a45b7c3bea702b"].into(),
+				// Pre-funded accounts
+				testnet_endowed_accounts(),
+				true,
+			)
+		},
+		// Bootnodes
+		vec![],
+		// Telemetry
+		Some(
+			TelemetryEndpoints::new(vec![(POLKADOT_TELEMETRY_URL.to_string(), 0)])
+				.expect("Polkadot telemetry url is valid; qed"),
+		),
+		None,
+		None,
+		// Properties
+		Some(fs_properties()),
+		// Extensions
+		None,
+	))
+}
+
+
 /// Configure initial storage state for FRAME modules.
 fn testnet_genesis(
 	wasm_binary: &[u8],
@@ -190,10 +266,8 @@ fn testnet_genesis(
 		transaction_payment: Default::default(),
 
 		role_module: RoleModuleConfig {
-			new_admin: Some(get_account_id_from_seed::<sr25519::Public>("Alice")),
+			new_admin: Some(hex!["2a0170a78af6835dd46753c1857b31903aa125d9c203e05bc7a45b7c3bea702b"].into()),
 			representatives: vec![
-				//get_account_id_from_seed::<sr25519::Public>("Gabriel"),
-				//get_account_id_from_seed::<sr25519::Public>("Henry"),
 			],
 		},
 		nft_module: NftModuleConfig {
@@ -206,9 +280,63 @@ fn testnet_genesis(
 		treasury: Default::default(),
 		council: CouncilConfig {
 			members: vec![
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				get_account_id_from_seed::<sr25519::Public>("Bob"),
-				get_account_id_from_seed::<sr25519::Public>("Charlie"),
+				hex!["2a0170a78af6835dd46753c1857b31903aa125d9c203e05bc7a45b7c3bea702b"].into(),
+				hex!["184a89cbb6aa857b41c98841be365ab3947ef1f729aa6fe0f6a1322f6391945b"].into(),
+				hex!["9ec0e63219270075ffd546e4fa39b4027216a9de5ed16b38bc54d66fe09b8d47"].into(),
+			],
+			phantom: Default::default(),
+		},
+		assets: Default::default(),
+	}
+}
+
+
+fn square_one(
+	wasm_binary: &[u8],
+	initial_authorities: Vec<(AuraId, GrandpaId)>,
+	root_key: AccountId,
+	endowed_accounts: Vec<(AccountId, u128)>,
+	_enable_println: bool,
+) -> GenesisConfig {
+	GenesisConfig {
+		system: SystemConfig {
+			// Add Wasm runtime to storage.
+			code: wasm_binary.to_vec(),
+		},
+		balances: BalancesConfig {
+			// Configure endowed accounts with initial balance of 1 << 60.
+			balances: endowed_accounts,
+		},
+		aura: AuraConfig {
+			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+		},
+		grandpa: GrandpaConfig {
+			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
+		},
+		sudo: SudoConfig {
+			// Assign network admin rights.
+			key: Some(root_key.clone()),
+		},
+		transaction_payment: Default::default(),
+
+		role_module: RoleModuleConfig {
+			new_admin: Some(hex!["2a0170a78af6835dd46753c1857b31903aa125d9c203e05bc7a45b7c3bea702b"].into()),
+			representatives: vec![
+			],
+		},
+		nft_module: NftModuleConfig {
+			owner: Some(root_key),
+			collection_id: Some(1),
+			created_by: Some(pallet_roles::Accounts::SERVICER),
+			metadata: Some(b"metadata".to_vec().try_into().unwrap()),
+		},
+		democracy: Default::default(),
+		treasury: Default::default(),
+		council: CouncilConfig {
+			members: vec![
+				hex!["2a0170a78af6835dd46753c1857b31903aa125d9c203e05bc7a45b7c3bea702b"].into(),
+				hex!["184a89cbb6aa857b41c98841be365ab3947ef1f729aa6fe0f6a1322f6391945b"].into(),
+				hex!["9ec0e63219270075ffd546e4fa39b4027216a9de5ed16b38bc54d66fe09b8d47"].into(),
 			],
 			phantom: Default::default(),
 		},
