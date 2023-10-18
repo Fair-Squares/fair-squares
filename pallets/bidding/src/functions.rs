d65b361999d70fb9bb480bcc24d865e7eb117fab
ndkazu <ndongmefane@gmail.com>
2023-10-18 15:23:32 +0900

    bidding process

--- pallets/bidding/src/functions.rs
+++ pallets/bidding/src/functions.rs
@@ -1,6 +1,6 @@
 pub use super::*;
-use rand::Rng;
-use rand::distributions::Slice;
+use rand::{Rng, seq::IteratorRandom};
+use rand::seq::SliceRandom;
 use sp_runtime::{BoundedVec,Percent};
 use frame_support::pallet_prelude::ConstU32;
 use Onboarding::Zero;
@@ -13,6 +13,7 @@ impl<T: Config> Pallet<T> {
         //get asset price
         let asset = Onboarding::Pallet::<T>::houses(collection_id,item_id).unwrap();
         let asset_price = asset.price.unwrap();
+        let mut remaining_amount = asset_price.clone();
         //Max contribution from asset Price
         //Min contribution from asset price
         
@@ -25,13 +26,17 @@ impl<T: Config> Pallet<T> {
             let contribution = Self::u128_to_onboarding_bal(contribution0).unwrap();
             //user contributed more than 5% of asset_price
             contribution > Percent::from_percent(5) * asset_price //should be minimun contribution calculated from asset price.
-            //true
+            //ToDo: We also want to only include users that did not contributed to a purchase for y_blocks (to be defined). 
 
         });
         
-        //Get a random sample of qualified investors 
-        let inv_dist = Slice::new(&investors).unwrap();
+        //Get a random sample of qualified investors
+        let mut rng = rand::thread_rng();
+
+        //Build a sample of 10 investors
+        let inv_dist = investors.choose_multiple(&mut rng, 10);        
         //let inv:BoundedVec<T,ConstU32<5>> = BoundedVec::truncate_from(inv_dist);
+
     }
 
     // Conversion of BalanceOf<T> to u128
